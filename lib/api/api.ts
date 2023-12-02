c4fd198b6743ff8a2d013ee516b4f10d77c1665a
duongthptnt <duongthptnt@gmail.com>
2023-12-02 13:09:03 +0700

    change to arrow function in  api.ts

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,17 +1,17 @@
-import { logger as Logger } from '../log'
+import { logger as Logger } from "../log";
 
 import {
-	ILogger,
-	ILoginResultAPI,
-	IAPIRequest,
-	IMessage,
-	ICredentials
-} from '../../interfaces'
+  ILogger,
+  ILoginResultAPI,
+  IAPIRequest,
+  IMessage,
+  ICredentials,
+} from "../../interfaces";
 
-import { Message } from '../message'
+import { Message } from "../message";
 
-import { EventEmitter } from 'tiny-events'
-import * as settings from '../settings';
+import { EventEmitter } from "tiny-events";
+import * as settings from "../settings";
 
 /** Check for existing login */
 // export function loggedIn () {
@@ -19,9 +19,9 @@ import * as settings from '../settings';
 // }
 
 /**
-	* Prepend protocol (or put back if removed from env settings for driver)
-	* Hard code endpoint prefix, because all syntax depends on this version
-	*/
+ * Prepend protocol (or put back if removed from env settings for driver)
+ * Hard code endpoint prefix, because all syntax depends on this version
+ */
 // export const url = `${(host.indexOf('http') === -1) ? host.replace(/^(\/\/)?/, 'http://') : host}/api/v1/`
 
 /** Populate auth headers (from response data on login) */
@@ -78,229 +78,257 @@ import * as settings from '../settings';
 // }
 
 export interface IClient {
-  headers: any
-  get (url: string, data: any, options?: any): Promise<any>
-  post (url: string, data: any, options?: any): Promise<any>
-  put (url: string, data: any, options?: any): Promise<any>
-  delete (url: string, data: any, options?: any): Promise<any>
+  headers: any;
+  get(url: string, data: any, options?: any): Promise<any>;
+  post(url: string, data: any, options?: any): Promise<any>;
+  put(url: string, data: any, options?: any): Promise<any>;
+  delete(url: string, data: any, options?: any): Promise<any>;
 }
 
 class Client implements IClient {
-  host: string
+  host: string;
 
-  _headers: any = {}
+  _headers: any = {};
 
-  constructor ({ host = 'http://localhost:3000' }: any) {
-    this.host = host
+  constructor({ host = "http://localhost:3000" }: any) {
+    this.host = host;
   }
 
-  set headers (obj: any) {
-    this._headers = obj
+  set headers(obj: any) {
+    this._headers = obj;
   }
-  get headers (): any {
+  get headers(): any {
     return {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       ...settings.customHeaders,
-      ...this._headers
-    }
+      ...this._headers,
+    };
   }
 
-  getHeaders (options?: any) {
-    return options && options.customHeaders ?
-      options.customHeaders :
-      this.headers
+  getHeaders(options?: any) {
+    return options && options.customHeaders
+      ? options.customHeaders
+      : this.headers;
   }
 
-  getBody (data: any) {
-    return data instanceof FormData ?
-      data :
-      JSON.stringify(data)
+  getBody(data: any) {
+    return data instanceof FormData ? data : JSON.stringify(data);
   }
 
-  getSignal (options?: any): AbortSignal {
+  getSignal(options?: any): AbortSignal {
     return options && options.signal;
   }
 
-  get (url: string, data: any, options?: any): Promise<any> {
-    return fetch(`${this.host}/api/v1/${encodeURI(url)}?${this.getParams(data)}`, {
-      method: 'GET',
-      headers: this.getHeaders(options),
-      signal: this.getSignal(options)
-    }).then(this.handle)
+  get(url: string, data: any, options?: any): Promise<any> {
+    return fetch(
+      `${this.host}/api/v1/${encodeURI(url)}?${this.getParams(data)}`,
+      {
+        method: "GET",
+        headers: this.getHeaders(options),
+        signal: this.getSignal(options),
+      }
+    ).then(this.handle);
   }
-  post (url: string, data: any, options?: any): Promise<any> {
+  post(url: string, data: any, options?: any): Promise<any> {
     return fetch(`${this.host}/api/v1/${encodeURI(url)}`, {
-      method: 'POST',
+      method: "POST",
       body: this.getBody(data),
       headers: this.getHeaders(options),
-      signal: this.getSignal(options)
-    }).then(this.handle)
+      signal: this.getSignal(options),
+    }).then(this.handle);
   }
-  put (url: string, data: any, options?: any): Promise<any> {
+  put(url: string, data: any, options?: any): Promise<any> {
     return fetch(`${this.host}/api/v1/${encodeURI(url)}`, {
-      method: 'PUT',
+      method: "PUT",
       body: this.getBody(data),
       headers: this.getHeaders(options),
-      signal: this.getSignal(options)
-    }).then(this.handle)
+      signal: this.getSignal(options),
+    }).then(this.handle);
   }
 
-  delete (url: string, data?: any, options?: any): Promise<any> {
+  delete(url: string, data?: any, options?: any): Promise<any> {
     return fetch(`${this.host}/api/v1/${encodeURI(url)}`, {
-      method: 'DELETE',
+      method: "DELETE",
       body: this.getBody(data),
       headers: this.getHeaders(options),
-      signal: this.getSignal(options)
-    }).then(this.handle)
+      signal: this.getSignal(options),
+    }).then(this.handle);
   }
-  private async handle (r: any) {
-    const { status } = r
-    const data = await r.json()
+  private async handle(r: any) {
+    const { status } = r;
+    const data = await r.json();
 
-    return { status, data }
-
+    return { status, data };
   }
-  private getParams (data: any) {
-    return Object.keys(data).map(function (k) {
-      return encodeURIComponent(k) + '=' + (typeof data[k] === 'object' ? encodeURIComponent(JSON.stringify(data[k])) : encodeURIComponent(data[k]))
-    }).join('&')
+  private getParams(data: any) {
+    return Object.keys(data)
+      .map(function (k) {
+        return (
+          encodeURIComponent(k) +
+          "=" +
+          (typeof data[k] === "object"
+            ? encodeURIComponent(JSON.stringify(data[k]))
+            : encodeURIComponent(data[k]))
+        );
+      })
+      .join("&");
   }
 }
 
-export const regExpSuccess = /(?!([45][0-9][0-9]))\d{3}/
+export const regExpSuccess = /(?!([45][0-9][0-9]))\d{3}/;
 
 /**
-	* @module API
-	* Provides a base client for handling requests with generic Rocket.Chat's REST API
-	*/
+ * @module API
+ * Provides a base client for handling requests with generic Rocket.Chat's REST API
+ */
 
 export default class Api extends EventEmitter {
-  userId: string = ''
-  logger: ILogger
-  client: IClient
+  userId: string = "";
+  logger: ILogger;
+  client: IClient;
   currentLogin: {
-    username: string,
-    userId: string,
-    authToken: string,
-    result: ILoginResultAPI
-  } | null = null
-  controller: AbortController
+    username: string;
+    userId: string;
+    authToken: string;
+    result: ILoginResultAPI;
+  } | null = null;
+  controller: AbortController;
 
-  constructor ({ client, host, logger = Logger }: any) {
-    super()
-    this.client = client || new Client({ host } as any)
-    this.logger = Logger
+  constructor({ client, host, logger = Logger }: any) {
+    super();
+    this.client = client || new Client({ host } as any);
+    this.logger = Logger;
     this.controller = new AbortController();
   }
 
-  get username () {
-    return this.currentLogin && this.currentLogin.username
+  get username() {
+    return this.currentLogin && this.currentLogin.username;
   }
 
-  loggedIn () {
-    return Object.keys(this.currentLogin || {} as any).every((e: any) => e)
+  loggedIn() {
+    return Object.keys(this.currentLogin || ({} as any)).every((e: any) => e);
   }
-/**
-	* Do a request to an API endpoint.
-	* If it needs a token, login first (with defaults) to set auth headers.
-	* @param method   Request method GET | POST | PUT | DEL
-	* @param endpoint The API endpoint (including version) e.g. `chat.update`
-	* @param data     Payload for POST request to endpoint
-	* @param auth     Require auth headers for endpoint, default true
-	* @param ignore   Allows certain matching error messages to not count as errors
-	*/
+  /**
+   * Do a request to an API endpoint.
+   * If it needs a token, login first (with defaults) to set auth headers.
+   * @param method   Request method GET | POST | PUT | DEL
+   * @param endpoint The API endpoint (including version) e.g. `chat.update`
+   * @param data     Payload for POST request to endpoint
+   * @param auth     Require auth headers for endpoint, default true
+   * @param ignore   Allows certain matching error messages to not count as errors
+   */
   request = async (
-		method: 'POST' | 'GET' | 'PUT' | 'DELETE',
-		endpoint: string,
-		data: any = {},
-		auth: boolean = true,
+    method: "POST" | "GET" | "PUT" | "DELETE",
+    endpoint: string,
+    data: any = {},
+    auth: boolean = true,
     ignore?: RegExp,
     options?: any
-	) => {
-    this.logger && this.logger.debug(`[API] ${ method } ${ endpoint }: ${ JSON.stringify(data) }`)
+  ) => {
+    this.logger &&
+      this.logger.debug(`[API] ${method} ${endpoint}: ${JSON.stringify(data)}`);
     try {
       if (auth && !this.loggedIn()) {
-        throw new Error('')
+        throw new Error("");
       }
 
       const { signal } = this.controller;
       options = { ...options, signal };
 
-      let result
+      let result;
       switch (method) {
-        case 'GET': result = await this.client.get(endpoint, data, options); break
-        case 'PUT': result = await this.client.put(endpoint, data, options); break
-        case 'DELETE': result = await this.client.delete(endpoint, data, options); break
+        case "GET":
+          result = await this.client.get(endpoint, data, options);
+          break;
+        case "PUT":
+          result = await this.client.put(endpoint, data, options);
+          break;
+        case "DELETE":
+          result = await this.client.delete(endpoint, data, options);
+          break;
         default:
-        case 'POST': result = await this.client.post(endpoint, data, options); break
+        case "POST":
+          result = await this.client.post(endpoint, data, options);
+          break;
       }
-      if (!result) throw new Error(`API ${ method } ${ endpoint } result undefined`)
-      if (!this.success(result, ignore)) throw result
-      this.logger && this.logger.debug(`[API] ${method} ${endpoint} result ${result.status}`)
-      const hasDataInsideResult = result && !result.data
-      return (method === 'DELETE') && hasDataInsideResult ? result : result.data
+      if (!result)
+        throw new Error(`API ${method} ${endpoint} result undefined`);
+      if (!this.success(result, ignore)) throw result;
+      this.logger &&
+        this.logger.debug(
+          `[API] ${method} ${endpoint} result ${result.status}`
+        );
+      const hasDataInsideResult = result && !result.data;
+      return method === "DELETE" && hasDataInsideResult ? result : result.data;
     } catch (err) {
-      this.logger && this.logger.error(`[API] POST error(${ endpoint }): ${ JSON.stringify(err) }`)
-      throw err
+      this.logger &&
+        this.logger.error(
+          `[API] POST error(${endpoint}): ${JSON.stringify(err)}`
+        );
+      throw err;
     }
-  }
-	/** Do a POST request to an API endpoint. */
-  post: IAPIRequest = (endpoint, data, auth, ignore, options = {}) => this.request('POST', endpoint, data, auth, ignore, options)
+  };
+  /** Do a POST request to an API endpoint. */
+  post: IAPIRequest = (endpoint, data, auth, ignore, options = {}) =>
+    this.request("POST", endpoint, data, auth, ignore, options);
 
-	/** Do a GET request to an API endpoint. */
-  get: IAPIRequest = (endpoint, data, auth, ignore, options = {}) => this.request('GET', endpoint, data, auth, ignore, options)
+  /** Do a GET request to an API endpoint. */
+  get: IAPIRequest = (endpoint, data, auth, ignore, options = {}) =>
+    this.request("GET", endpoint, data, auth, ignore, options);
 
-	/** Do a PUT request to an API endpoint. */
-  put: IAPIRequest = (endpoint, data, auth, ignore, options = {}) => this.request('PUT', endpoint, data, auth, ignore, options)
+  /** Do a PUT request to an API endpoint. */
+  put: IAPIRequest = (endpoint, data, auth, ignore, options = {}) =>
+    this.request("PUT", endpoint, data, auth, ignore, options);
 
-	/** Do a DELETE request to an API endpoint. */
-  del: IAPIRequest = (endpoint, data, auth, ignore, options = {}) => this.request('DELETE', endpoint, data, auth, ignore, options)
+  /** Do a DELETE request to an API endpoint. */
+  del: IAPIRequest = (endpoint, data, auth, ignore, options = {}) =>
+    this.request("DELETE", endpoint, data, auth, ignore, options);
 
   /** Abort all current API requests. */
-  abort = (): void => this.controller.abort()
+  abort = (): void => this.controller.abort();
 
-	/** Check result data for success, allowing override to ignore some errors */
-  success (result: any, ignore?: RegExp) {
-    return (
-			typeof result.status === 'undefined' ||
-			(result.status && regExpSuccess.test(result.status)) ||
-			(result.status && ignore && ignore.test(result.status))
-		) ? true : false
+  /** Check result data for success, allowing override to ignore some errors */
+  success(result: any, ignore?: RegExp) {
+    return typeof result.status === "undefined" ||
+      (result.status && regExpSuccess.test(result.status)) ||
+      (result.status && ignore && ignore.test(result.status))
+      ? true
+      : false;
   }
 
-  async login (credentials: ICredentials, args?: any): Promise<any> {
-    const { data } = await this.post('login', { ...credentials, ...args })
-    this.userId = data.userId
+  login = async (credentials: ICredentials, args?: any): Promise<any> => {
+    const { data } = await this.post("login", { ...credentials, ...args });
+    this.userId = data.userId;
     this.currentLogin = {
       username: data.me.username,
       userId: data.userId,
       authToken: data.authToken,
-      result: data
-    }
+      result: data,
+    };
     this.client.headers = {
-      'X-Auth-Token': data.authToken,
-      'X-User-Id': data.userId
-    }
-    return data
-  }
-  async logout () {
+      "X-Auth-Token": data.authToken,
+      "X-User-Id": data.userId,
+    };
+    return data;
+  };
+  logout = async () => {
     if (!this.currentLogin) {
-      return null
+      return null;
     }
-    const result = await this.post('logout', {}, true)
-    this.userId = ''
-    this.currentLogin = null
-    return result
-  }
-/**
- * Structure message content, optionally addressing to room ID.
- * Accepts message text string or a structured message object.
- */
-  prepareMessage (
-	content: string | IMessage,
-	rid?: string,
-	args?: any
-): Message {
-    return new Message(content, { rid, roomId: rid, ...args })
+    const result = await this.post("logout", {}, true);
+    this.userId = "";
+    this.currentLogin = null;
+    return result;
+  };
+  /**
+   * Structure message content, optionally addressing to room ID.
+   * Accepts message text string or a structured message object.
+   */
+  prepareMessage(
+    content: string | IMessage,
+    rid?: string,
+    args?: any
+  ): Message {
+    return new Message(content, { rid, roomId: rid, ...args });
   }
 }
